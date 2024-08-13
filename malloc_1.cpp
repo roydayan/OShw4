1cc88ba12c242a6a05bb6be81934575ad7cbcf19
roydayan <roy.dayan@campus.technion.ac.il>
2024-08-13 20:01:54 +0300

    smalloc after passing simple test

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -7,23 +7,35 @@
 
 void* smalloc(size_t size){
     if (size == 0 || size > MAX_SIZE) {
-        return NULL;
+        return nullptr; //Note - this is .cpp file, so I am using nullptr instead of NULL
     }
     void* alloc_ptr = sbrk(static_cast<intptr_t>(size)); //TODO - should I use C-style cast??? sbrk((intptr_t)size);
     if (alloc_ptr == (void*)(-1)) {
-        return NULL;
+        return nullptr;
     }
     return alloc_ptr; //TODO - does this return the first allocated bit as desired or the last bit from previous block??
 }
 
+/*
 //--------------------------------
-//FOR TEST
+//FOR TEST - Note: passed this
 //--------------------------------
 int main() {
     int i = 1000;
     void* ptr = smalloc(i);
-    if (ptr == NULL) {
+    if (ptr == nullptr) {
         return 1;
+    }
+    int *int_arr = (int*)ptr;
+    int x = int_arr[0];
+    int y = int_arr[i-1];
+    for (int j = 0; j < i; j++) {
+        int_arr[j] = j;
+    }
+    int z = int_arr[i-1];
+    if (y == z) { //make sure value changed
+        return 2;
     }
     return 0;
 }
+*/
